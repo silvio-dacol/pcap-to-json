15a2f5c4ab1b902dac34985091cbc30fb397b435
Silvio Da Col <silvio.dacol@outlook.com>
2025-08-21 06:36:10 +0200

    Added index to the packet

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use pcap::Capture;
 
 #[derive(Serialize)]
 struct PacketRecord {
+    index: usize,
     timestamp_sec: i64,
     timestamp_usec: i64,
     packet_len: u32,
@@ -25,14 +26,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut cap = Capture::from_file(&in_path)?;
     let mut out = File::create(out_path)?;
 
+    // Initialize index to the display the packet number
+    let mut index = 0;
+
     // Process each packet
     while let Ok(packet) = cap.next_packet() {
+        // Increment the index every processed packet
+        index = index + 1;
+
         let record = PacketRecord {
+            index: index,
             timestamp_sec: packet.header.ts.tv_sec as i64,
             timestamp_usec: packet.header.ts.tv_usec as i64,
             packet_len: packet.header.len,
             raw_hex: hex_string(packet.data),
         };
+
+        // Write the JSON record to the output file
         writeln!(out, "{}", serde_json::to_string(&record)?)?;
     }
 
