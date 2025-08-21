82b0759f8097ac7fe1115f771fda4316def596d4
Silvio Da Col <silvio.dacol@outlook.com>
2025-08-21 09:44:00 +0200

    UTC Time OK

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use std::{env, fs::File, io::Write};
 use serde::Serialize;
 use pcap::Capture;
+use chrono::{DateTime, Utc};
 
 #[derive(Serialize)]
 struct PacketRecord {
     index: usize,
-    timestamp_sec: i64,
-    timestamp_usec: i64,
+    timestamp_sec: String,
     packet_len: u32,
     raw_hex: String,
+}
+
+// Function to parse the timestamp into a UTC string with format %H:%M:%S.%f
+fn utc_time(sec: i64, usec: i64) -> String {
+    let dt = DateTime::<Utc>::from_timestamp(sec, usec as u32).unwrap();
+    dt.format("%Y-%m-%d %H:%M:%S.%f").to_string()
 }
 
 fn hex_string(data: &[u8]) -> String {
@@ -26,7 +32,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut cap = Capture::from_file(&in_path)?;
     let mut out = File::create(out_path)?;
 
-    // Initialize index to the display the packet number
+    // Initialize index that displays the packet number
     let mut index = 0;
 
     // Process each packet
@@ -36,8 +42,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         let record = PacketRecord {
             index: index,
-            timestamp_sec: packet.header.ts.tv_sec as i64,
-            timestamp_usec: packet.header.ts.tv_usec as i64,
+            timestamp_sec: utc_time(packet.header.ts.tv_sec as i64, packet.header.ts.tv_usec as i64),
             packet_len: packet.header.len,
             raw_hex: hex_string(packet.data),
         };
